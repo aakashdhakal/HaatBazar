bcadf410610014881a7f04cb5c803c42de7c182d
Aakash Dhakal <anamoldhakal22@gmail.com>
2025-04-12 10:12:35 +0545

    Remove unused imports and create collection for Order model in getAllProducts API route

--- src/app/(server)/api/getAllProducts/route.jsx
+++ src/app/(server)/api/getAllProducts/route.jsx
@@ -2,21 +2,6 @@
 import dbConnect from "@/lib/db";
 import Product from "@/modals/productModal";
 import getAllProducts from "@/app/(server)/actions/products";
-import { get } from "mongoose";
-
-// // //----------------------------------------------------------
-// import User from "../@/modals/userModal";
-// import Account from "../@/modals/accountModal";
-// // import Session from "@/modals/sessionModal";
-// // import VerificationToken from "@/modals/verificationTokenModal";
-import Order from "@/modals/orderModal";
-
-// // Account.createCollection();
-// // Session.createCollection();
-// // VerificationToken.createCollection();
-Order.createCollection();
-
-// // //----------------------------------------------------------
 
 import { NextResponse } from "next/server";
 
